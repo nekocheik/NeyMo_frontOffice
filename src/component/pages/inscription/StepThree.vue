dc5adc47b56cfb0db1f7944a6e4120ddc4c1cd46
cheik <cheik.kone@hetic.net>
2020-06-21 03:22:06 +0200

    end step three of inscription

--- src/component/pages/inscription/StepThree.vue
+++ src/component/pages/inscription/StepThree.vue
@@ -1,8 +1,16 @@
 <template>
   <ValidationObserver ref="observer">
     <div class="mt-10">
+      <ValidationProvider name="age" rules="required|min:10" v-slot="{ errors }">
+        <m-input :errors="errors" type="tel" v-model="birthDay" mask="##/##/##" maxlength="10" exemple="30/09/1998">Age</m-input>
+      </ValidationProvider>
       <ValidationProvider name="Nom" rules="required|min:3" v-slot="{ errors }">
-        <m-input :errors="errors" v-model="firstName" exemple="Uzumaki">Nom</m-input>
+        <m-input :errors="errors" type="tel" v-model="phoneNumber" mask="## ## ## ## ##" maxlength="14" exemple="06 21 48 37 99">Téléphone</m-input>
+      </ValidationProvider>
+      <ValidationProvider name="mail" rules="email|required" v-slot="{ errors }">
+        <m-input :errors="errors" v-model="mail" exemple="marie.jane@mail.com">
+          Email
+        </m-input>
       </ValidationProvider>
     </div>
   </ValidationObserver>
@@ -12,8 +20,9 @@ export default {
   name: 'StepOne',
   data() {
     return {
-      firstName: '',
-      lastName: '',
+      birthDay: '',
+      phoneNumber: '',
+      mail: '',
     };
   },
   mounted() {
